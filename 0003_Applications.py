1f3fa54b5d22e91649e7c3fb5213f920cbd7d0ed
Marcos Ferreira <ferreira.mr@pm.me>
2022-02-23 13:43:30 -0300

    Dia 002

--- 0003_Applications.py
+++ 0003_Applications.py
@@ -1,9 +1,11 @@
+from loguru import logger
 from starlette.applications import Starlette
 from starlette.responses import PlainTextResponse
 from starlette.routing import Route, Mount, WebSocketRoute
 from starlette.staticfiles import StaticFiles
 
 def homepage(request):
+    logger.info(request.app.state.HELLO)
     return PlainTextResponse('Hello, world!')
 
 def user_me(request):
@@ -31,3 +33,4 @@ routes = [
 ]
 
 app = Starlette(debug=True, routes=routes, on_startup=[startup])
+app.state.HELLO = "Hello World!"
